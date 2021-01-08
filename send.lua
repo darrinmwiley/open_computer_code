2bb47c2b773b76ebd4ba556f0fd47ce2488d78e3
darrinmwiley <darrinmwiley@gmail.com>
2021-01-08 01:24:49 -0600

    Create send.lua

---
+++ send.lua
@@ -0,0 +1,25 @@
+local addr_tab = {}
+local arr = {}
+local ct = 1
+
+for line in io.lines("addr_db") do
+  arr[ct] = line
+  ct = ct + 1
+end
+
+for i = 1,ct-1, 2 do
+  addr_tab[arr[i]] = arr[i+1]
+  
+local component = require("component")
+local event = require("event")
+local m = component.modem
+m.open(123)
+
+args = {...}
+
+name = args[1]
+message = args[2]
+
+m.send(addr_tab[name], message)
+  
+  
