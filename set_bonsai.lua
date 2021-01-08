bb68d8f1980c93547584d1e0264e2752d655b86a
darrinmwiley <darrinmwiley@gmail.com>
2021-01-08 01:17:44 -0600

    Update set_bonsai.lua

--- set_bonsai.lua
+++ set_bonsai.lua
@@ -2,18 +2,9 @@ function set_bonsai(name)
   local bonsai_control = require("bonsai_control")
   local utils = require("utils")
   
-  local addr_tab = {}
-  local arr = {}
-  local ct = 1
-  
-  for line in io.lines("addr_db") do
-    arr[ct] = line
-    ct = ct + 1
-  end
-  
   sapling_tab = {}
   
-  ct = 1
+  local ct = 1
   for line in io.lines("sapling_db") do
     sapling_tab[line] = ct
     ct = ct + 1
