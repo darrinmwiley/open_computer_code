a087d21b589563e0b2ffadaffb03971dc9c5a7e3
darrinmwiley <darrinmwiley@gmail.com>
2021-01-08 00:07:20 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,5 @@
 local utils = require("utils")
 local component = require("component")
+local set_bonsai = require("set_bonsai")
 
-local export_bus = component.me_exportbus
-local database = component.database
-
-db_addr = utils.getAddress("database")
-export_bus.setExportConfiguration(5,1,db_addr,1)
+set_bonsai.setBonsai("redstone")
