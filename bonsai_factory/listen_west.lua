967ce1df4651e7bfedc44b27e03ab900dd95dace
darrinmwiley <darrinmwiley@gmail.com>
2021-01-12 01:43:15 -0600

    Update listen_west.lua

--- bonsai_factory/listen_west.lua
+++ bonsai_factory/listen_west.lua
@@ -6,22 +6,26 @@ local set_bonsai = require("set_bonsai")
 local component = require("component")
 local event = require("event")
 local m = component.modem
-m.open(123)
+local redstone = component.redstone
+redstone.setOutput(break_dir, 15)
+redstone.setOutput(place_dir, 15)
 
 args = {...}
+
+port = tonumber(args[1])
+m.open(port)
 
 function lpad(str, len, char)
     return string.rep(char, len - #str) .. str
 end
 
-id = lpad(args[1],3,"0")
+id = lpad(args[2],3,"0")
 
 while true do
   local _, _, from, port, _, message = event.pull("modem_message")
-  set_bonsai.setBonsai(tostring(message))
-  req_id = string.sub(message,0,3)
+  req_id = string.sub(message,1,3)
   if req_id == id then
-    req_resource = string.sub(message, 4)
+    req_resource = string.sub(message, 5)
     print(req_resource)
     set_bonsai.setBonsai(place_dir, break_dir, bus_dir, req_resource)
   end
