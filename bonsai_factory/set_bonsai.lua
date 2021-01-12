1c96b2aeed55d06b5266b472ea209e9b63eb3b82
darrinmwiley <darrinmwiley@gmail.com>
2021-01-12 01:11:59 -0600

    Update set_bonsai.lua

--- bonsai_factory/set_bonsai.lua
+++ bonsai_factory/set_bonsai.lua
@@ -1,1 +1,23 @@
+function set_bonsai(place_dir, break_dir, bus_dir, resource)
+  local utils = require("utils")
+  local component = require("component")
+  local sap = require("db_util")
+  local redstone = component.redstone
+  local bus = component.me_exportbus
+  
+  db_addr = utils.getAddress("database")
 
+  index = sap[resource]
+  
+  if index ~= nil then
+    bus.setExportConfiguration(bus_dir,1,db_addr, n)
+    --precondition of the redstone default outputs being on for place_dir and break_dir
+    redstone.setOutput(break_dir, 0)
+    os.sleep(.021)
+    redstone.setOutput(break_dir, 15)
+    os.sleep(.021)
+    redstone.setOutput(place_dir, 0)
+    os.sleep(.021)
+    redstone.setOutput(place_dir,15)
+  end
+end
